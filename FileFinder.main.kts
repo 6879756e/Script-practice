cdfaf273287eebb35fb802e040465d07bdc8ade4
6879756e <6879756e@gmail.com>
2021-10-11 10:09:15 +0900

    Implement FileFinder basic
    Currently searches one directory only
    TODO: search nested directories

---
+++ FileFinder.main.kts
@@ -0,0 +1,73 @@
+import java.io.File
+
+private val pwd = File("")
+private val S = "    "
+private val fileType by lazy { args[0] }
+private val verbose: Boolean
+    get() = if (args.size > 1) args[1] == "v" else false
+
+main()
+
+fun pwd(): String {
+    try {
+        val filePath = File("").absoluteFile
+        return "This script is being called from the following directory: $filePath"
+    } catch (e: SecurityException) {
+        println("A required system property value cannot be accessed.")
+        throw e
+    }
+}
+
+fun main() {
+    if (args.isEmpty()) {
+        printManualUsage()
+        return
+    }
+    printAllFilesInFolder()
+}
+
+fun printManualUsage() {
+    println(
+        """
+FileFinder(1)                   6879756e General Commands Manual                  FileFinder(1)
+NAME
+    FileFinder -- finds all files of a specific types in the directory and all its subdirectories
+    
+SYNOPSIS
+    kotlinc -script FileFinder.main.kts [string ...]"
+    
+DESCRIPTION
+    The file finder, as the name suggests, helps find files of a particular type.
+    
+    The following options are available:
+    
+    -f    The file type e.g. jpg
+    
+    -v    Lists all files (default is 5)
+    
+    """
+    )
+
+}
+
+fun printAllFilesInFolder() {
+    val filesList = pwd.absoluteFile.list()
+    println("Searching in the follow directory: ${pwd.absoluteFile}")
+
+    val sb = StringBuilder()
+
+    filesList?.filter { it.endsWith(fileType) }?.forEach { sb.appendLine("$S$it") }
+
+    val result = when {
+        sb.isEmpty() -> "No files of the type: $fileType exists"
+        (verbose || sb.lines().size < 5) -> sb.lines().sorted().joinToString("\n")
+        else -> {
+            sb.lines().take(5).sorted().joinToString("\n") +
+                    "\n$S...\n" +
+                    "As there were a lot of results, we only listed the first 5!\n" +
+                    "Tip: Use the -v flag to list all results!"
+        }
+    }
+
+    println(result)
+}
